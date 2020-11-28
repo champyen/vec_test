88437c13f1328161cbef274877aca6be5a5f9e84
Champ Yen <champ.yen@gmail.com>
2020-11-28 17:38:52 +0800

    to approach concrete vector usage

--- vec_test.cpp
+++ vec_test.cpp
@@ -21,17 +21,35 @@ int main(void)
 
     srand(time(NULL));
 
-    for(int i = 0; i < 1024*1024; i++){
+    for(int i = 0; i < 1024*1024+1024; i++){
         vec1.emplace_back(rand());
         vec2.emplace_back();
         vec3.emplace_back(rand(), rand());
         vec4.emplace_back(rand());
+
+        if(i > 1024){
+            for(int j = 0; j < 6; j++){
+                int idx = rand() % i;
+                vec2[idx].emplace_back(rand());
+            }
+        }
     }
 
+    printf("capacity %lu %lu %lu %lu %lu %lu\n",
+        vec1.capacity(),
+        vec2.capacity(),
+        vec3.capacity(),
+        vec4.capacity(),
+        vec5.capacity(),
+        vec6.capacity()
+    );
+
     // if the next 3 lines are commented out, Pss value of each process will decrease.
+    #if 1
     vector<uint64_t>().swap(vec1);
     vector<vector<uint64_t>>().swap(vec2);
     vector<pair<uint64_t, uint64_t>>().swap(vec3);
+    #endif
 
     printf("Before fork[%d] %u KB\n", getpid(), getpss(getpid()));
 
@@ -40,7 +58,7 @@ int main(void)
         if(pid == 0) break;
     }
 
-    for(int i = 0; i < 1024*1024; i++){
+    for(int i = 0; i < 1024*1024+1024; i++){
         vec5.emplace_back(rand());
         vec6.emplace_back(rand());
     }
